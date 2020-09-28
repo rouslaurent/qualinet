3d7ba10245c53aa5398dd291fd8acf076ff6b887
laurentrous <55280818+laurentrous@users.noreply.github.com>
2020-09-28 14:53:06 +0200

    feat: authentification

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+       $user = new User;
+       $user->setUsername('lolo');
+       $user->setPassword($this->encoder->encodePassword($user, 'lolo'));
+       $manager->persist($user);
+       $manager->flush();
+    }
+}
